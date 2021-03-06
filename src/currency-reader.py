5587bc3bc32f17a3cc2211ffd57f5de67e0f67b1
Jesper Fussing Mørk <jfm@moerks.dk>
2021-03-06 15:07:01 +0100

    Trying to push something to kafka

--- src/currency-reader.py
+++ src/currency-reader.py
@@ -1,10 +1,22 @@
+from kafka import KafkaProducer
+import os
 import falcon
+import json
 
 
 class HelloWorldResource(object):
+
+    def __init__(self):
+        self.producer = KafkaProducer(
+                value_serializer=lambda v: json.dumps(v).encode("utf-8"), 
+                bootstrap_servers=os.getenv['KAFKA_BOOTSTRAP_SERVERS', 'localhost:1324']
+                )
+
     def on_get(self, request, response):
+        self.producer.send('testQueue', {"data": "Hello World"})
+
         response.status = falcon.HTTP_200
-        response.body = '{"hello": "all"}'
+        response.body = 'Message Sent'
 
 
 app = falcon.API()
